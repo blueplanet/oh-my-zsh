e2621a5a56eeb90a786454d1cda805e66891ffbe
blueplanet <erguolinge@gmail.com>
2012-04-09 22:38:13 +0900

    alias

---
+++ custom/custom-aliases.zsh
@@ -0,0 +1,31 @@
+# base command
+alias ls='ls -GwF'
+alias ll='ls -l'
+alias la='ll -a'
+
+# git
+alias g='git'
+alias gs='g st'
+alias gl='g log'
+alias gll='gl --oneline --graph'
+alias gf='g flow'
+alias gfs='gf feature start'
+alias gff='gf feature finish'
+
+# editor
+alias e='vi'
+alias o='open'
+
+# ruby / rails
+alias r='rails'
+alias rs='rake spec'
+
+# tmux
+alias ti='tmuxinator'
+alias tis='ti start'
+alias tio='ti open'
+alias tik='tmux kill-session'
+
+# Emacs
+alias e='/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
+
